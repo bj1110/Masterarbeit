9971e34a60d42155c4576a5670f3de9ed480c0db
bjoern <bjoern.97@web.de>
2025-12-09 17:02:37 +0100

    created a solver node

--- ros2_ws/src/ik_processing/src/solver.cpp
+++ ros2_ws/src/ik_processing/src/solver.cpp
@@ -1,11 +1,39 @@
-// #include <rclcpp/rclcpp.hpp>
-// #include <geometry_msgs/msg/quaternion.hpp>
-// #include <sensor_msgs/msg/joint_state.hpp>
-// #include <tf2_ros/transform_broadcaster.h>
-// #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-// #include <cmath>
-// #include <thread>
-// #include <chrono>
-// #include <moveit/robot_model_loader/robot_model_loader.hpp>
-// #include <moveit/robot_model/robot_model.hpp>
-// #include <moveit/robot_state/robot_state.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.hpp>
+#include <memory>
+
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions node_options;
+    node_options.automatically_declare_parameters_from_overrides(true);
+    const auto move_group_node = std::make_shared<rclcpp::Node>("solver", node_options);
+
+    const auto LOGGER = rclcpp::get_logger("solver");
+
+    static const std::string PLANNING_GROUP = "arm"; 
+
+    moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP);
+
+    geometry_msgs::msg::Pose target_pose = move_group.getCurrentPose().pose;
+    // move_group.setStartStateToCurrentState(); 
+    // auto const target_pose = []{
+    // geometry_msgs::msg::Pose msg;
+    //     msg.orientation.w = 1.0;
+    //     msg.position.x = 0.39;
+    //     msg.position.y = 0.22;
+    //     msg.position.z = 0.93;
+    //     return msg;
+    // }();
+    move_group.setPoseTarget(target_pose);
+    
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    
+    bool success = (move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+
+
+    RCLCPP_INFO(LOGGER, "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
+
+
+    rclcpp::shutdown();
+    return 0;
+}
