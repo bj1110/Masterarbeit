8ede4968a3b1184ec065e3ba6c433fbc4387dfdb
bjoern <bjoern.97@web.de>
2025-12-12 17:01:01 +0100

    introduced moveit modeling for model3. Collision problems to be fixed next

---
+++ ros2_ws/src/config_movit_model3/launch/move_group.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_move_group_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("human_arm_model3", package_name="config_moveit_model3").to_moveit_configs()
+    return generate_move_group_launch(moveit_config)
