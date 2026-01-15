4578599e3890c519f2d62a286a0d57811b304b67
bjoern <bjoern.97@web.de>
2026-01-15 17:25:37 +0100

    trying to fix orientation during movement

--- ros2_ws/src/ik_processing/src/fullsim.cpp
+++ ros2_ws/src/ik_processing/src/fullsim.cpp
@@ -195,6 +195,9 @@ int main(int argc, char** argv)
 
     */
 
+    /*
+        der Punkt 1 liegt bei ca x= -25 und y=-220
+    */
 
     // Start the demo
     // ^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -210,22 +213,31 @@ int main(int argc, char** argv)
 
     Datapoint dp1 = data[0];
 
+    float roll = -2.47;
+    float pitch = 0.0; 
+    float yaw = -1.57;
     tf2::Quaternion q;
-    q.setRPY(0.0, M_PI , 0.0);  // roll, pitch, yaw
+    q.setRPY(roll, pitch , yaw);  // roll, pitch, yaw
     q.normalize();
 
+
+
     
+    RCLCPP_INFO(LOGGER, "First Dataset: t=%f s, x= %f mm, y=%f mm, z=%f mm", dp1.time, dp1.x1, dp1.y1, dp1.z1); 
+
+    // Aufgrund der Achsenwahl: tausche x und y 
     geometry_msgs::msg::Pose target_pose1;
-    target_pose1.orientation.w=1; 
-    // target_pose1.orientation = tf2::toMsg(q);
-    target_pose1.position.x = (dp1.y1 +300)/1000;
-    target_pose1.position.y = (dp1.x1 +300)/1000;
-    target_pose1.position.z = (dp1.z1 +477)/1000;
+    // target_pose1.orientation.y=-0.69; 
+    target_pose1.orientation = tf2::toMsg(q);
+    target_pose1.position.x = (dp1.y1 +220 +250 )/1000;
+    target_pose1.position.y = (dp1.x1)/1000;
+    target_pose1.position.z = (dp1.z1 +690)/1000;
     move_group.setPoseTarget(target_pose1);
     
-    move_group.setGoalOrientationTolerance(0.25);
-    // // RCLCPP_INFO(LOGGER, "First Target Pose: w=%f, x=%f, x=%f, z=%f",target_pose1.orientation.w, 
-    // //  target_pose1.position.x,  target_pose1.position.y,  target_pose1.position.z);
+    move_group.setGoalOrientationTolerance(1);
+
+    RCLCPP_INFO(LOGGER, "First Target Pose: w=%f, x=%f m, y=%f m , z=%f m ",target_pose1.orientation.w, 
+        target_pose1.position.x,  target_pose1.position.y,  target_pose1.position.z);
 
     // // geometry_msgs::msg::Pose target_pose2;
     // // target_pose2.position.x = 0.28;
@@ -260,32 +272,32 @@ int main(int argc, char** argv)
     // visual_tools.publishTrajectoryLine(my_plan.trajectory, joint_model_group);
     // visual_tools.trigger();
 
-    std::vector<geometry_msgs::msg::Pose> waypoints;
-    for(auto dp: data){
-        if(dp==data[1] || dp==data[2]){
-            continue; 
-        }
-        geometry_msgs::msg::Pose p;
-        p.position.x = (dp.y1 +300)/1000;
-        p.position.y = (dp.x1 +300)/1000;
-        p.position.z = (dp.z1 +477)/1000;
-        waypoints.push_back(p);
-    }
+    // std::vector<geometry_msgs::msg::Pose> waypoints;
+    // for(auto dp: data){
+    //     if(dp==data[1] || dp==data[2]){
+    //         continue; 
+    //     }
+    //     geometry_msgs::msg::Pose p;
+    //     p.position.x = (dp.y1 +300)/1000;
+    //     p.position.y = (dp.x1 +300)/1000;
+    //     p.position.z = (dp.z1 +477)/1000;
+    //     waypoints.push_back(p);
+    // }
 
-    const double eef_step = 0.01;
-    moveit_msgs::msg::RobotTrajectory trajectory;
-    moveit_msgs::msg::Constraints path_contraints;
-    const bool avoid_collisions=true; 
-    moveit_msgs::msg::MoveItErrorCodes* errorcode = nullptr; 
+    // const double eef_step = 0.01;
+    // moveit_msgs::msg::RobotTrajectory trajectory;
+    // moveit_msgs::msg::Constraints path_contraints;
+    // const bool avoid_collisions=true; 
+    // moveit_msgs::msg::MoveItErrorCodes* errorcode = nullptr; 
 
-    double fraction = move_group.computeCartesianPath(waypoints, eef_step, trajectory, /*path_contraints, */ avoid_collisions, errorcode);
-    RCLCPP_INFO(LOGGER, "Number of Waypoints: %ld", waypoints.size());
-    RCLCPP_INFO(LOGGER, "Visualizing plan 4 (Cartesian path) (%.2f%% achieved)", fraction * 100.0);
-    if(errorcode && errorcode->val != moveit::core::MoveItErrorCode::SUCCESS){
-        RCLCPP_INFO(LOGGER, "Error in Carthesian Path: "); 
-        RCLCPP_INFO(LOGGER, "Error in Carthesian Path: %s", (errorcode->message).c_str());
-    }
-    move_group.execute(trajectory); 
+    // double fraction = move_group.computeCartesianPath(waypoints, eef_step, trajectory, /*path_contraints, */ avoid_collisions, errorcode);
+    // RCLCPP_INFO(LOGGER, "Number of Waypoints: %ld", waypoints.size());
+    // RCLCPP_INFO(LOGGER, "Visualizing plan 4 (Cartesian path) (%.2f%% achieved)", fraction * 100.0);
+    // if(errorcode && errorcode->val != moveit::core::MoveItErrorCode::SUCCESS){
+    //     RCLCPP_INFO(LOGGER, "Error in Carthesian Path: "); 
+    //     RCLCPP_INFO(LOGGER, "Error in Carthesian Path: %s", (errorcode->message).c_str());
+    // }
+    // move_group.execute(trajectory); 
     
     /*
         Creating the Path towards the desired output folder:
